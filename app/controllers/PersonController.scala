9fa6f86847709fed66f94203917033f943536786
Noah Kaplan <nnk96@hotmail.com>
2016-08-05 11:44:01 -0400

    Removed unnecessary comments

--- app/controllers/PersonController.scala
+++ app/controllers/PersonController.scala
@@ -28,8 +28,6 @@ class PersonController @Inject() (repo: PersonRepository, val messagesApi: Messa
 
   val genderCheckConstraint: Constraint[String] = Constraint("constraints.genderscheck")({
     str =>
-      // you have access to all the fields in the form here and can
-      // write complex logic here
       if (str == "M" || str == "F") {
         Valid
       } else {
@@ -119,35 +117,21 @@ class PersonController @Inject() (repo: PersonRepository, val messagesApi: Messa
     }
   }
 
-  /**
-   * The add person action.
-   *
-   * This is asynchronous, since we're invoking the asynchronous methods on PersonRepository.
-   */
   def addPerson = Action.async { implicit request =>
-    // Bind the form first, then fold the result, passing a function to handle errors, and a function to handle succes.
     personForm.bindFromRequest.fold(
-      // The error function. We return the index page with the error form, which will render the errors.
-      // We also wrap the result in a successful future, since this action is synchronous, but we're required to return
-      // a future because the person creation function returns a future.
       errorForm => {
         repo.list().map { people =>
           Ok(views.html.index(errorForm)(people.length)(jsonForm))
         }
       },
-      // There were no errors in the from, so create the person.
       person => {
         repo.create(person.name, person.age, person.gender).map { _ =>
-          // If successful, we simply redirect to the index page.
           Redirect(routes.PersonController.index)
         }
       }
     )
   }
 
-  /**
-   * A REST endpoint that gets all the people as JSON.
-   */
   def getPersons = Action.async {
   	repo.list().map { people =>
       Ok(Json.toJson(people))
@@ -155,13 +139,6 @@ class PersonController @Inject() (repo: PersonRepository, val messagesApi: Messa
   }
 }
 
-/**
- * The create person form.
- *
- * Generally for forms, you should define separate objects to your models, since forms very often need to present data
- * in a different way to your models.  In this case, it doesn't make sense to have an id parameter in the form, since
- * that is generated once it's created.
- */
 case class CreatePersonForm(name: String, age: Int, gender: String)
 
 case class CreateJsonForm(json: String)
