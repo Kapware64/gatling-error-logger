1d127ef5160513db377ee5dc548a8e41eb17dc7b
Noah Kaplan <nnk96@hotmail.com>
2016-08-05 11:45:39 -0400

    Removed unnecessary comments

--- app/dal/PersonRepository.scala
+++ app/dal/PersonRepository.scala
@@ -8,11 +8,6 @@ import models.Person
 
 import scala.concurrent.{ Future, ExecutionContext }
 
-/**
- * A repository for people.
- *
- * @param dbConfigProvider The Play db config provider. Play will inject this for you.
- */
 @Singleton
 class PersonRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
   private val dbConfig = dbConfigProvider.get[JdbcProfile]
@@ -20,44 +15,16 @@ class PersonRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(impl
   import dbConfig._
   import driver.api._
 
-  /**
-   * Here we define the table. It will have a name of people
-   */
   private class PeopleTable(tag: Tag) extends Table[Person](tag, "people") {
-
-    /** The ID column, which is the primary key, and auto incremented */
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
-
-    /** The name column */
     def name = column[String]("name")
-
-    /** The age column */
     def age = column[Int]("age")
-
     def gender = column[String]("gender")
-
-    /**
-     * This is the tables default "projection".
-     *
-     * It defines how the columns are converted to and from the Person object.
-     *
-     * In this case, we are simply passing the id, name and page parameters to the Person case classes
-     * apply and unapply methods.
-     */
     def * = (id, name, age, gender) <> ((Person.apply _).tupled, Person.unapply)
   }
 
-  /**
-   * The starting point for all queries on the people table.
-   */
   private val people = TableQuery[PeopleTable]
-
-  /**
-   * Create a person with the given name and age.
-   *
-   * This is an asynchronous operation, it will return a future of the created person, which can be used to obtain the
-   * id for that person.
-   */
+  
   def create(name: String, age: Int, gender: String): Future[Int] = {
     val q = people += Person(0, name, age, gender)
     db.run(q)
@@ -72,9 +39,6 @@ class PersonRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(impl
     people.delete
   }
 
-  /**
-   * List all the people in the database.
-   */
   def list(): Future[Seq[Person]] = db.run {
     people.result
   }
